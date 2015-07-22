509649dea794c71084e0fcb7429959eff7913cad
Wimsy113 <Krystyna.d.l.ewing@gmail.com>
2015-07-22 09:54:10 -0400

    changed logger configuration

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+#Log levels DO NOT CHANGE
+config.log_level = :fatal
+Rails.logger.level = 0
+
 # Initialize the Rails application.
 Rails.application.initialize!
