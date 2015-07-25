fd2be10d4e00bebb2b1ef92188edc40bf5dbd820
Wimsy113 <Krystyna.d.l.ewing@gmail.com>
2015-07-25 14:17:05 -0400

    built up css for welcome index, as well as edited config file due to issues with logs

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 #Log levels DO NOT CHANGE
-config.log_level = :fatal
-Rails.logger.level = 0
+
 
 # Initialize the Rails application.
 Rails.application.initialize!
